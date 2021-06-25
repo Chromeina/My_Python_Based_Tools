0131345a9bea1043d417f066f4d6d4119c0b0c51
Chromeina <58142866+Chromeina@users.noreply.github.com>
2021-06-25 15:56:17 +0800

    Update LRC_Extract.py

--- LRC_Extract.py
+++ LRC_Extract.py
@@ -2,11 +2,13 @@ from os import listdir
 from os.path import join, isfile, splitext
 
 path = input('請輸入要處理的路徑: ')
+# path = input('Enter the path to process: ')
 path = path.replace("\\", '/')
 file_list = listdir(path)
 
 for file_name in file_list:
     print('正在處理: ' + file_name)
+    # print('Processing: ' + file_name)
     full_path = join(path, file_name)
 
     if file_name.endswith('.flac'):
@@ -28,11 +30,14 @@ for file_name in file_list:
             tail_position = music_data.find(b'\x00\x00\x00\x74\x69\x74\x6C\x65')
             if head_position == -1 or tail_position == -1:
                 print(file_name + ' 沒有歌詞檔案!')
+                # print(file_name + ' doesn't contain lyrics!')
             else:
                 lyrics_file = open(full_path[:-extension_length] + 'lrc', 'wb')
                 lyrics_file.write(music_data[head_position+7:tail_position-1])
                 lyrics_file.close()
         except ValueError:
             print(file_name + ' 沒有歌詞檔案!')
+            # print(file_name + ' doesn't contain lyrics!')
     else:
         print(file_name + ' 不是音樂檔案!')
+        # print(file_name + ' isn't a music file!')
